ad42144749fb83ce78de2d759aa4765de26b66ab
srikanthpragada <srikanthpragada@gmail.com>
2020-08-06 20:58:52 +0530

    06-AUG

---
+++ cmd.txt.py
@@ -0,0 +1,30 @@
+# coding: utf-8
+datetime.now()
+print('Welcome To Jupyter Notebook')
+from datetime import datetime
+datetime.now()
+a = 10 
+# Print squares of number from 1 to 10
+for i in range(1,11):
+    print(i*i)
+datetime.now()
+get_ipython().run_line_magic('pinfo', 'datetime.strptime')
+get_ipython().run_line_magic('pinfo', 'datetime.now')
+from datetime import timedelta
+t = timedelta(day = 10)
+from datetime import timedelta
+t = timedelta(hours= 10)
+## Markdown Demo
+<ul>
+<li>Numpy</li>
+<li>Pandas</li>
+</ul>
+<div class="alert alert-block alert-info">
+This is alert box
+</div>
+from IPython.display import Image
+Image(url ="http://www.srikanthtechnologies.com/logo.jpg")
+from IPython.display import Image
+Image(url ="http://www.srikanthtechnologies.com/banner.jpg")
+<img src="http://www.srikanthtechnologies.com/banner.jpg"/>
+get_ipython().system('cd')
